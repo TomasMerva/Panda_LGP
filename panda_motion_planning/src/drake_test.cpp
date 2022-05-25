b13d3ee6f9463f026fbe8ac00f671511e6f08825
TomasMerva <tmerva7@gmail.com>
2022-05-25 22:12:38 +0200

    drake test

--- panda_motion_planning/src/drake_test.cpp
+++ panda_motion_planning/src/drake_test.cpp
@@ -1,8 +1,5 @@
 #include <ros/ros.h>
 
-#include <ifopt/problem.h>
-#include <ifopt/ipopt_solver.h>
-#include <panda_motion_planning/simple_optim_mp.h>
 
 #include <drake/solvers/mathematical_program.h>
 // #include <drake/solvers/solve.h>
@@ -11,39 +8,46 @@
 #include <cmath>
 #include <typeinfo>
 
-using namespace ifopt;
 
 using drake::solvers::MathematicalProgram;
-using drake::solvers;
+using drake::solvers::IpoptSolver;
+
+
+typedef Eigen::Matrix<double, 1, Eigen::Dynamic> RowVector;
+
+
+#include <chrono>
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "ifopt_mp");
     ros::NodeHandle nh;
 
-    int num_states = 3;
-    int num_collocation_points = 10;
-
-    MathematicalProgram program;
-    // auto x = program.NewContinuousVariables(num_collocation_points, num_states, "joints");
-    
+    MathematicalProgram program;    
     auto x = program.NewContinuousVariables(2);
     program.AddConstraint(x[0] + x[1] == 1);
     program.AddConstraint(x[0] <= x[1]);
     program.AddCost(pow(x[0],2) + pow(x[1],2));
 
     IpoptSolver solver;
-    Eigen::VectorXd initial_guess;
+    RowVector initial_guess(2);
     initial_guess << 1, 1;
-    auto result = solver.Solve(program, initial_guess, 0);
+        
+    auto t1 = high_resolution_clock::now();
+    auto result = solver.Solve(program, initial_guess);
+    auto t2 = high_resolution_clock::now();
 
-    
+    std::cout << result.is_success() << "\n";
+    std::cout << result.GetSolution(x) << "\n";
+    std::cout << result.get_solver_id().name() << std::endl;
 
-
-    // std::cout << result.is_success() << "\n";
-    // std::cout << result.GetSolution(x) << "\n";
-    // std::cout << result.get_solver_id().name() << std::endl;
-
+    duration<double, std::milli> ms_double = t2 - t1;
+    std::cout << ms_double.count() << "ms\n";
 
     return 0;
 }
