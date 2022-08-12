9ec2461441407cb6e2476bb0ba447bcb9a0549ec
TomasMerva <tmerva@rocketmail.com>
2022-08-12 13:48:41 +0200

    orient J v1

--- panda_inverse_kinematics/src/constraints/orientation_constraint.cpp
+++ panda_inverse_kinematics/src/constraints/orientation_constraint.cpp
@@ -15,8 +15,8 @@ Eigen::VectorXd OrientationConstraint::GetValues() const
     VectorXd g(GetRows());
     VectorXd x = GetVariables()->GetComponent("x")->GetValues();
     auto FK_q = Kinematics::ForwardKinematics(x, true);
-    Eigen::MatrixXd R_WA = FK_q.block<3,3>(0,0);
-    auto R_WB = Kinematics::RotationMatrixFromRPY(_rpy[0], _rpy[1], _rpy[2]);
+    Eigen::MatrixXd R_WA = FK_q.block<3,3>(0,0);    // Rotation matrix of the current q configuration
+    auto R_WB = Kinematics::RotationMatrixFromRPY(_rpy[0], _rpy[1], _rpy[2]); // The desired rotation matrix
     auto R_AB = R_WA.transpose() * R_WB;
     g(0) = R_AB.trace();
     return g;
@@ -32,5 +32,32 @@ OrientationConstraint::VecBound OrientationConstraint::GetBounds() const
 
 void OrientationConstraint::FillJacobianBlock (std::string var_set, Jacobian& jac_block) const
 {
+    if (var_set == "x") 
+    {
+        VectorXd x = GetVariables()->GetComponent("x")->GetValues();
+        auto FK_q = Kinematics::ForwardKinematics(x, true);
+        Eigen::MatrixXd R_WA = FK_q.block<3,3>(0,0);
+        auto R_WB = Kinematics::RotationMatrixFromRPY(_rpy[0], _rpy[1], _rpy[2]);
+        auto R_AB = R_WA.transpose() * R_WB;
+        
+        const Eigen::Vector3d r_AB{R_AB(1, 2) - R_AB(2, 1), R_AB(2, 0) - R_AB(0, 2),
+                                   R_AB(0, 1) - R_AB(1, 0)};
+        Eigen::MatrixXd Jq_w_AB(3, 7);
 
+
+        double delta_q = 0.001;
+        for (int i=0; i<7; i++)
+        {
+            auto temp = x;
+            temp(i) += delta_q; // ad delta_q to the q(i)
+            auto FK_with_deltaq = Kinematics::ForwardKinematics(temp, true); //compute FK with q(i)+delta_q
+            Eigen::MatrixXd R_WA_with_deltaq = FK_with_deltaq.block<3,3>(0,0);
+            auto R_AB = R_WA.transpose() * R_WB;
+
+            Jq_w_AB(0, i) = (FK_with_deltaq(0,3) - FK_q(0,3)) / delta_q;  // dx / dq(i)
+            Jq_w_AB(1, i) = (FK_with_deltaq(1,3) - FK_q(1,3)) / delta_q;  // dy / dq(i)
+            Jq_w_AB(2, i) = (FK_with_deltaq(2,3) - FK_q(2,3)) / delta_q;  // dz / dq(i)
+        }
+
+    }
 }
